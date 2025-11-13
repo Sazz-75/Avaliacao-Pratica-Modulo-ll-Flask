520318562bf04246695a5f4964e38774c226cc09
Sazz-75 <samuelvictoralves0@gmail.com>
2025-11-13 18:21:05 +0000

    Samuel victor alves dias modulo 2       tarde

--- app_flask.py
+++ app_flask.py
@@ -9,5 +9,47 @@ def index():
 
 ## Continue o código aqui.
 
+# soma
+
+@app.route("/soma")
+def somar():
+    valor_a = request.args.get("valor1",type=int)
+    valor_b = request.args.get("valor2",type=int)
+    total = valor_a + valor_b
+    return {"resultado": total}
+
+
+
+# subtração
+
+@app.route("/subtracao")
+def subtrair():
+    valor_a = request.args.get("valor1",type=int)
+    valor_b = request.args.get("valor2",type=int)
+    total = valor_a - valor_b
+    return {"resultado": total}
+
+# divisão
+
+@app.route("/divisao")
+def dividir():
+    valor_a = request.args.get("valor1",type=int)
+    valor_b = request.args.get("valor2",type=int)
+    if valor_b == 0:
+        return "erro de divisao por zero"
+    total = valor_a / valor_b
+    return {"resultado" : total}
+
+
+# multiplicação
+
+@app.route("/multiplicacao")
+def multiplicar():
+    valor_a = request.args.get("valor1",type=int)
+    valor_b = request.args.get("valor2",type=int)
+    total = valor_a * valor_b
+    return {"resultado": total}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
